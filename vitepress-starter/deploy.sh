b7e32b4efac90a8b971a6166de5bcdf975ce5695
许小墨 <1440023258@qq.com>
2024-02-14 00:14:26 +0800

    fix: 部署失败

--- vitepress-starter/deploy.sh
+++ vitepress-starter/deploy.sh
@@ -14,6 +14,6 @@ git add -A
 git commit -m 'deploy'
 
 # 如果发布到 https://<USERNAME>.github.io/<REPO>
-git push -f git@github.com:mondaylab/mondaylab-blog.git master:gh-pages
+git push -f git@github.com:Angelxyj/xuxiaomo.guide.git main:gh-pages
 
 cd -
